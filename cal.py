5fb2dfb068dd6e7fbd19acf76a6fb29c41f31fd0
nick <kipletingbor2@gmail.com>
2020-07-07 13:35:48 +0100

    change

--- cal.py
+++ cal.py
@@ -12,3 +12,37 @@ print(d.size)
 f=np.arange(20,100,10).reshape(2,4)
 print(f)
 print(f.ndim)
+
+h= np.linspace(20,40,15).reshape(3,5)
+print(h)
+
+k=np.random.rand(40)
+print(k)
+
+j=np.arange(10000).reshape(100,100)
+print(j)
+#basic operation
+l=np.linspace(100,400,10)
+q=np.linspace(50,100,10)
+c=l-q
+print(c)
+c=l+q
+print(c)
+c=l*q
+d=10*np.sin(c)
+print(d>-5)
+w=d.reshape(2,5)
+print(w)
+g=l.reshape(5,2)
+
+
+##
+w=np.arange(10)
+print(np.sqrt(w))
+# indexing and slicing 
+s=np.arange(10)**3
+print(s)
+print(s[3:5])
+for i in s:
+    print(i**(1/3.))
+    
